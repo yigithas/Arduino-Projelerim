15c77c082df3f454a60d500794d3d8ead71690ff
yigithas <ygthas@gmail.com>
2026-01-20 01:29:00 +0300

    Update and rename kodlar.c.txt to kodlar.c

---
+++ Arduino/Led ile Çakar Yapımı/kodlar.c
@@ -0,0 +1,19 @@
+int ledler[] = {2,3,4,5,6,7};
+
+void setup() {
+for(int i=0;i<6;i++){
+ 
+ pinMode(ledler[i],OUTPUT);
+
+}
+
+}
+
+void loop() {
+
+  for(int i=0;i<6;i++){
+    digitalWrite(ledler[i],HIGH);
+    delay(50);
+    digitalWrite(ledler[i],LOW);
+  }
+}
